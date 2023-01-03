f5cdd537a02e0111c50731437d5266b957dc3d5f
yonasketema <yonask.kitaw@gmail.com>
2023-01-03 20:30:37 +0300

    add Screen component

---
+++ app/components/Screen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Platform, SafeAreaView, StatusBar, StyleSheet } from "react-native";
+
+function Screen({ children }) {
+  return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+});
+export default Screen;
