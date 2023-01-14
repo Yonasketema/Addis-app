771571d6f031cb632d5ec73f924d6808eb7733bb
yonasketema <yonask.kitaw@gmail.com>
2023-01-14 11:15:26 +0300

    add foodDetailScreen

--- app/screens/FoodDetailScreen.js
+++ app/screens/FoodDetailScreen.js
@@ -1,4 +1,8 @@
 import { StyleSheet, View, Image, Text, StatusBar } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+
 import Screen from "../components/Screen";
 
 function FoodDetailScreen() {
@@ -6,40 +10,101 @@ function FoodDetailScreen() {
     <Screen>
       <StatusBar translucent backgroundColor="rgba(0,0,0,.4)" />
 
-      <View>
+      <View style={{ flex: 1 }}>
         <Image
           style={styles.image}
           source={{
-            uri: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80",
+            uri: "http://cdn.cnn.com/cnnnext/dam/assets/190205150343-kitfo.jpg",
           }}
         ></Image>
+        <View style={styles.like_box}>
+          <AntDesign name="heart" size={20} color="red" />
+          <Text style={{ color: "#fff", marginLeft: 5 }}>1123</Text>
+        </View>
         <View style={styles.description}>
-          <Text style={styles.title}>spaghetti with spaicy mixed seaFood</Text>
-          <Text style={styles.subTitle}>$100</Text>
+          <View style={styles.titlebar}>
+            <Text style={styles.title} numberOfLines={2}>
+              spaghetti with spaicy mixed seaFood
+            </Text>
+            <Text style={styles.price}>$100</Text>
+          </View>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Ionicons name="ios-time-outline" size={24} color="black" />
+            <Text style={styles.time}>10 minutes</Text>
+          </View>
+          <Text style={{ color: "#999", marginVertical: 5 }}>
+            They had to maintain programs with millions of lines of code. Before
+            they could test new changes, they had to compile the code into a
+            runnable form, a process which at the time took the better part of
+            an hour.
+          </Text>
+          <View style={{ position: "absolute", bottom: 10, right: 10 }}>
+            <View
+              style={{
+                backgroundColor: "black",
+                width: 80,
+                height: 80,
+                borderRadius: 40,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Feather name="navigation" size={40} color="#fff" />
+            </View>
+          </View>
         </View>
       </View>
     </Screen>
   );
 }
 const styles = StyleSheet.create({
+  like_box: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    position: "absolute",
+    top: 20,
+    right: 20,
+    backgroundColor: "rgba(0,0,0,0.4)",
+    padding: 10,
+    borderRadius: 10,
+  },
   image: {
     width: "100%",
     height: 300,
   },
   description: {
-    marginVertical: 7,
-    paddingHorizontal: 15,
+    padding: 15,
     width: "100%",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    position: "absolute",
+    backgroundColor: "#fff",
+    bottom: 0,
+    height: "50%",
+  },
+  navigator: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   title: {
     fontWeight: "bold",
-    fontSize: 18,
-    color: "#222",
+    fontSize: 20,
+    width: "75%",
+    color: "#333",
     marginBottom: 10,
+  },
+  titlebar: {
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   subTitle: {
     color: "blue",
     marginBottom: 25,
+  },
+  price: {
+    fontSize: 17,
+    fontWeight: "bold",
   },
 });
 
