48092e867a32dd60d1fcd613bc0e90afc8a5e44e
yonasketema <yonask.kitaw@gmail.com>
2023-02-16 20:45:40 +0300

    config intro slider

--- App.js
+++ App.js
@@ -1,22 +1,124 @@
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AppIntroSlider from "react-native-app-intro-slider";
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Button,
+} from "react-native";
 
 import FoodDetailScreen from "./app/screens/FoodDetailScreen";
 import HomeScreen from "./app/screens/HomeScreen";
 
 const Stack = createNativeStackNavigator();
 
+const StackNavigator = () => (
+  <Stack.Navigator
+    screenOptions={{
+      headerShown: false,
+    }}
+  >
+    <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Screen name="Detail" component={FoodDetailScreen} />
+  </Stack.Navigator>
+);
+
 export default function App() {
+  const [showRealApp, setShowRealApp] = useState(false);
+
+  const onDone = () => {
+    setShowRealApp(true);
+  };
+
+  const onSkip = () => {
+    setShowRealApp(true);
+  };
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Detail" component={FoodDetailScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      {showRealApp ? (
+        <NavigationContainer>
+          <StackNavigator />
+        </NavigationContainer>
+      ) : (
+        <AppIntroSlider
+          data={slides}
+          renderItem={RenderItem}
+          onDone={onDone}
+          showSkipButton={true}
+          onSkip={onSkip}
+        />
+      )}
+    </>
   );
 }
+
+const RenderItem = ({ item }) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: item.backgroundColor,
+        alignItems: "center",
+        justifyContent: "space-around",
+        paddingBottom: 100,
+      }}
+    >
+      <Text style={styles.introTitleStyle}>{item.title}</Text>
+      <Image style={styles.introImageStyle} source={item.image} />
+      <Text style={styles.introTextStyle}>{item.text}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    padding: 10,
+    justifyContent: "center",
+  },
+  titleStyle: {
+    padding: 10,
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  paragraphStyle: {
+    padding: 20,
+    textAlign: "center",
+    fontSize: 16,
+  },
+  introImageStyle: {
+    width: "100%",
+    height: 200,
+  },
+  introTextStyle: {
+    fontSize: 18,
+    color: "#000",
+    textAlign: "center",
+    paddingVertical: 30,
+  },
+  introTitleStyle: {
+    fontSize: 25,
+    color: "#000",
+    textAlign: "center",
+    marginBottom: 16,
+    fontWeight: "bold",
+  },
+});
+
+const slides = [
+  {
+    key: "s1",
+    title: "Addis Food & Restorant",
+    text: "Enjoy Great offers on our all services",
+    image: require("./app/assets/4ufood.png"),
+    backgroundColor: "#DDD",
+  },
+];
